9f9c735385b69336299c5506bb3e0b82a2042f5a
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-10 00:46:31 +0600

    product change url

--- src/routes/[category]/[product]/+page.js
+++ src/routes/[category]/[product]/+page.js
@@ -1,9 +1,11 @@
 import {  redirect } from '@sveltejs/kit';
 import { product_url } from '../../../lib/store/store.js';
+import { changeProductURL } from '../../../lib/function/ChangeProductUrl.js';
 
 export const load =async ({params})=> {
     if(params.product){
-        product_url.set(params.product)
+        console.log("Setting params",params.product)
+        changeProductURL(params.product)
     }else{
        throw redirect(300,"/")
     }
