33936a758cf2c76ffee78c470a0c191d93a47231
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-10 00:54:02 +0600

    productt details page bug fixed

--- src/routes/[category]/[product]/+page.svelte
+++ src/routes/[category]/[product]/+page.svelte
@@ -1,21 +1,33 @@
 <script>
-	import { storeCartToLocalStorage } from './../../../lib/function/storeCartToLocalStorage.js';
-    import { allProductsData, product_url ,myCartData} from "../../../lib/store/store";
+	import { fetchCartData } from './../../../lib/function/fetchCardData.js';
+	import { onMount } from 'svelte';
+	import { addToCart } from './../../../lib/function/addToCart.js';
+    import { allProductsData, product_url ,myCartData, product_details} from "../../../lib/store/store";
     
-    const product = $allProductsData.filter(p=> p.p_url == $product_url)[0];
-    console.log(product)
-    const {p_id,p_name,p_url,cat_id,cat_name,p_price,p_img,p_type,p_reg_price,p_stock} = product;
-    $: matched = $myCartData.filter(i=> i.p_url == p_url)[0]
+    
+    
+    // let product = $product_details;
+    // console.log("product id is ", $product_url)
+    onMount(()=>{
+        fetchCartData()
+        // changeProductURL($product_url)
+    })
+    // const {p_id,p_name,p_url,cat_id,cat_name,p_price,p_img,p_type,p_reg_price,p_stock} = product;
+    // let {p_id,p_name,p_url,cat_id,cat_name,p_price,p_img,p_type,p_reg_price,p_stock} = $product_details;
+    // $: matched = $myCartData.filter(i=> i.p_id == product.p_id)[0]
+    $: matched = $myCartData.filter(i=> i.p_id == $product_details.p_id)[0]
+    console.log("Result of Match - ", matched)
 
     const handleAddtoCart = ()=> {
-        console.log("Result of Match - ", matched)
-        if(matched){
+        if(matched ){
             alert("Product already added to cart")
             return
         }else{
-            storeCartToLocalStorage(product)
-            myCartData.update(cart=> [...cart,product])
-            console.log("Cart Updated-", $myCartData)
+            addToCart( $product_details.cat_id, $product_details.cat_name,$product_details.p_id,$product_details.p_img,$product_details.p_name,$product_details.p_price,$product_details.p_reg_price,$product_details.p_stock,$product_details.p_type,$product_details.p_url)
+
+            // storeCartToLocalStorage(product)
+            // myCartData.update(cart=> [...cart,product])
+            // console.log("Cart Updated-", $myCartData)
         }
     }
 </script>
@@ -24,21 +36,22 @@
 <section class="flex items-center justify-center my-5">
     <div class="w-full  p-2 rounded  flex flex-col md:flex-row gap-5 justify-center ">
         <div class="w-full md:w-1/2 overflow-hidden rounded">
-            <img class="w-full hover:scale-110 transition-all ease-in-out" src={p_img} alt={p_name}>
+            <img class="w-full hover:scale-110 transition-all ease-in-out" src={$product_details.p_img} alt={$product_details.p_name}>
         </div>
         <div class="w-full md:w-1/2 p-3 py-5 md:py-10 font-sans text-sm flex gap-3 flex-col justify-between">
-            <h1 class="text-xl font-semibold">{p_name}</h1>
-            <p class="font-semibold">Regular Price: <span class=" text-xl line-through font-medium">${p_reg_price}</span></p>
-            <p class="font-semibold">Offer Price: <span class="text-xl text-green-500">${p_price}</span></p>
-            <p class="font-semibold">Type: {p_type}</p>
-            <p >Category: {cat_name}</p>
+            <h1 class="text-xl font-semibold">{$product_details.p_name}</h1>
+            <p class="font-semibold">Regular Price: <span class=" text-xl line-through font-medium">${$product_details.p_reg_price}</span></p>
+            <p class="font-semibold">Offer Price: <span class="text-xl text-green-500">${$product_details.p_price}</span></p>
+            <p class="font-semibold">Type: {$product_details.p_type}</p>
+            <p >Category: {$product_details.cat_name}</p>
             <div>
                 <p class="font-semibold">Description:</p>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga illum perspiciatis harum, mollitia quae animi, nobis sit asperiores repellat dolorum aliquid suscipit aperiam velit nam similique temporibus odit ullam magnam?</p>
             </div>
             {#if matched}
-                 <!-- content here -->
-                 <button disabled class="bg-blue-500 p-5 font-bold text-white rounded ">Product Already Added</button>
+                <button disabled class="bg-slate-500 p-5 font-bold text-white rounded ">
+                <a href="https://traxnyc-demo-main.vercel.app/cart">Product Already Added</a>
+                </button>
             {:else}
                  <button on:click={handleAddtoCart} class="bg-blue-500 p-5 font-bold text-white rounded hover:bg-blue-600">In my Shopping Cart</button>
             {/if}
