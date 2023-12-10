a9527344d92c349956491b8c9110932c703f00c6
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-10 08:03:10 +0600

    name updated

--- src/lib/components/home-components/HeaderSection.svelte
+++ src/lib/components/home-components/HeaderSection.svelte
@@ -6,7 +6,7 @@
 <div class=" relative flex flex-col gap-5 items-center min-h-[70vh]">
     <img class=" absolute top-0 w-full z-0" src="/banner.png" alt="banner">
     <div class="z-10 w-[90%] md:w-[75%] p-5 md:pl-16">
-        <h1 class="text-red-500 md:text-white mt-1 md:mt-20  text-2xl md:text-5xl font-extrabold md:font-bold">Shopping with <span class="text-red-500">TRAXNYC</span></h1>
+        <h1 class="text-red-500 md:text-white mt-1 md:mt-20  text-2xl md:text-5xl font-extrabold md:font-bold">Shopping with <span class="text-red-500">TRAXNYC Belgium</span></h1>
         <!-- <div class="flex gap-2 md:gap-5 md:items-center md:mt-10">
             <button class="flex items-center gap-2 bg-orange-200 rounded-full px-3 py-2 md:px-5 md:py-3">
                 <div class="text-lg md:text-3xl ">
