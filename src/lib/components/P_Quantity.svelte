45e51ca530925e3cab88e847ae15e6d0c77ea624
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-11 23:29:44 +0600

    logic updated

--- src/lib/components/P_Quantity.svelte
+++ src/lib/components/P_Quantity.svelte
@@ -1,14 +1,12 @@
 <script>
+	import { count_product } from './../function/count_product.js';
     import Icon from '@iconify/svelte';
-    export let p_price;
-    export let increaseTotalPrice;
-    export let decreaseTotalPrice;
     export let product;
     console.log("product is  ", product)
-    let qty = 1;
+    let qty = parseInt(product.p_quantity);
     const increaseQty = ()=>{
         qty++
-        increaseTotalPrice(parseInt(p_price),product)
+        count_product(product.p_id,qty )
     }
     const decreaseQty = ()=>{
         if(qty < 1){
@@ -16,22 +14,24 @@
         }else if(qty == 1){
             alert("Quantity cannot be less than 1")
             qty = 1
-            // decreaseTotalPrice(parseInt(p_price),qty)
+            count_product(product.p_id,qty )
         }else{
-            decreaseTotalPrice(parseInt(p_price),product)
             qty--
+            count_product(product.p_id,qty )
+
         }
     }
+
 </script>
-<div class="flex  items-center gap-2 justify-between w-full ">
-    <p class="font-bold ">Quantity : {qty} </p>
+<div class="flex flex-col items-center gap-2 justify-between w-full ">
+    <p class="font-bold ">Quantity :  {product.p_quantity} </p>
     <div class="flex gap-3 text-white py-3 font-bold">
         <button on:click={decreaseQty} class="px-3 py-1 md:px-5 bg-red-500 rounded flex items-center gap-2">
             <Icon icon="ic:baseline-minus" />
         </button>
-        <p class="text-black">
-            <!-- {qty} -->
-        </p>
+        <!-- <p class="text-black">
+            {qty}
+        </p> -->
         <button on:click={increaseQty} class="px-3 py-1 md:px-5 bg-green-500 rounded text-xl flex items-center gap-2">
             <Icon icon="ic:baseline-plus" />
         </button>
